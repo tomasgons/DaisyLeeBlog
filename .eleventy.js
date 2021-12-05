daa72ac801fcca8d1ce6413e4457641c7bd2fb8a
gonzocode <tomasgons@gmail.com>
2021-12-06 00:32:09 +0100

    refactoring

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
-const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toDateString(DateTime.DATE_MED);
-    })
+
 
     return {
         dir: {
