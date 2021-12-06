d6771486d5c4244884caee4acb75fcb8edccb7b4
gonzocode <tomasgons@gmail.com>
2021-12-06 01:56:25 +0100

    added cms

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
 
     return {
